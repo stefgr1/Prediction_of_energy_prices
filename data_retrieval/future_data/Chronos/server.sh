f7f82b40d079be9402a0e2f6064a6eadcc3c976b
stefgr1 <stefan.grochowski@gmx.de>
2024-10-16 10:44:32 +0200

    Added cluster jobs

--- data_retrieval/future_data/Chronos/server.sh
+++ data_retrieval/future_data/Chronos/server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --partition=dev_single        # Ensure this partition is valid
+#SBATCH --partition=single        # Ensure this partition is valid
 #SBATCH --nodes=1                   # Request a single node
 #SBATCH --cpus-per-task=8          # Specify the number of CPU cores for the task
 #SBATCH --mem=32GB              # Set memory for the job
-#SBATCH --time=00:30:00             # Set maximum runtime
+#SBATCH --time=08:00:00             # Set maximum runtime
 #SBATCH --output=job_output_%j.out
 #SBATCH --error=job_error_%j.err
 #SBATCH --job-name=Chro_OiL
