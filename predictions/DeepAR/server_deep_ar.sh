6f56bad0ba2a0960e91989019ae980223232df7f
stefgr1 <stefan.grochowski@gmx.de>
2024-11-03 12:03:38 +0100

    Minor changes

--- predictions/DeepAR/server_deep_ar.sh
+++ predictions/DeepAR/server_deep_ar.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --partition=gpu_4
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --nodes=1                     # Request 1 node
 #SBATCH --cpus-per-task=6             # 6 CPU cores per task
 #SBATCH --mem=8000mb                  # 12 GB of memory
 #SBATCH --time=05:00:00                 
-#SBATCH --job-name=Deep_AR_52  # Set the job name
+#SBATCH --job-name=DARnolag  # Set the job name
 #SBATCH --mail-user=zxoul27@student.uni-tuebingen.de  # Email notifications
 #SBATCH --mail-type=BEGIN,END,FAIL    # Email on job start, end, and fail
 
